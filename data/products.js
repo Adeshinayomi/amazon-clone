dd1fc34326ca048045dc5bec939c314d4b2d1009
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-04-09 19:01:29 +0100

    learn promises and covert code to promises

--- data/products.js
+++ data/products.js
@@ -106,14 +106,13 @@ export function loadProducts(fun){
         }
         return new Product(productDetails)
       });
-
+      console.log('load products');
       fun()
   })
   xhr.open('GET', 'https://supersimplebackend.dev/products')
   xhr.send()
 }
 
-loadProducts()
 // export const products = [
 //   {
 //     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,47 @@
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js'
+  Promise.all([
+    new Promise((resolve)=>{
+      loadProducts(()=>{
+        resolve('value1')
+      })
+    }),
+    new Promise((resolve)=>{
+      loadCart(()=>{
+        resolve()
+      })
+    })
 
+  ]).then((value)=>{
+    console.log(value)
+    renderOrderSummary();
+    renderPaymentSummary(); 
+  })
+
+// new Promise((resolve)=>{
+//   loadProducts(()=>{
+//     resolve('value1')
+//   })
+// }).then((value)=>{
+//   console.log(value)
+
+//   return new Promise((resolve)=>{
+//     loadCart(()=>{
+//       resolve()
+//     })
+//   })
+// }).then(()=>{
+//   renderOrderSummary();
+//   renderPaymentSummary(); 
+// })
+/*
 loadProducts(()=>{
-  renderOrderSummary();
-  renderPaymentSummary();
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPaymentSummary(); 
+  })
 })
+*/
